fa3fdba41b3c2a4188ec92973c7e898ae026ebf9
Nick Dobson <nick.dobson@me.com>
2015-04-15 23:23:07 -0500

    Empty cookbook

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'swarm'
+maintainer       'Nick Dobson'
+maintainer_email 'nick.dobson@me.com'
+license          'all_rights'
+description      'Installs/Configures swarm'
+long_description 'Installs/Configures swarm'
+version          '0.1.0'
+
+depends 'docker'
